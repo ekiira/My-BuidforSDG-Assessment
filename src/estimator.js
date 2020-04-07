d139ba2e6b606293370d8a7c6836c4b43f4af786
Tosin Jimoh <jayoluwatosin@gmail.com>
2020-04-07 23:56:34 +0100

    challenge-one

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,27 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data, durationInput) => {
+  let durationPeriod = 28;
+  if (durationInput) {
+    if (!Number.isInteger(durationInput)) {
+      throw new Error('Duration period is not a number');
+    }
+    durationPeriod = durationInput;
+  }
+
+  const impactReportedCase = data.reportedCases * 10;
+  const severeImpactReportedCase = data.reportedCases * 50;
+  const duration = 2 ** (Math.floor(durationPeriod / 3));
+
+  return {
+    data,
+    impact: {
+      currentlyInfected: impactReportedCase,
+      infectionsByRequestedTime: impactReportedCase * duration
+    },
+    severeImpact: {
+      currentlyInfected: severeImpactReportedCase,
+      infectionsByRequestedTime: severeImpactReportedCase * duration
+    }
+  };
+};
 
 export default covid19ImpactEstimator;
