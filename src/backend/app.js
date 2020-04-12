bdc8f63fb6efcf2d4535de7585e66684deccaf9e
Tosin Jimoh <jayoluwatosin@gmail.com>
2020-04-12 18:43:42 +0100

    updates

--- src/backend/app.js
+++ src/backend/app.js
@@ -5,8 +5,10 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+// const { check, validationResult } = require('express-validator');
 const convert = require('xml2js');
 const covid19ImpactEstimator = require('../estimator');
+
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -18,11 +20,13 @@ app.get('/', (req, res) => {
 app.post('/api/v1/on-covid-19', (req, res) => {
   const datum = req.body;
   const receive = (covid19ImpactEstimator(datum));
+  res.set('Cache-Control', 'max-age=31557600');
   res.send(receive);
 });
 
 app.post('/api/v1/on-covid-19/json', (req, res) => {
   const datum = req.body;
+  res.set('Cache-Control', 'max-age=31557600');
   res.send(covid19ImpactEstimator(datum));
 });
 
@@ -31,6 +35,7 @@ app.post('/api/v1/on-covid-19/xml', (req, res) => {
   res.type('application/xml');
   const builder = new convert.Builder();
   const xml = builder.buildObject(covid19ImpactEstimator(datum));
+  res.set('Cache-Control', 'max-age=31557600');
   res.send(xml);
 });
 
--- src/estimator.js
+++ src/estimator.js
@@ -1,10 +1,6 @@
 import getImpact from './impact';
 import getSevereImpact from './severeImpact';
 
-// const getSevereImpact = './severeImpact.js';
-// const getImpact = require('./impact');
-// const getSevereImpact = require('./severeImpact');
-
 const covid19ImpactEstimator = (data) => ({
   data,
   impact: getImpact(data),
@@ -12,21 +8,4 @@ const covid19ImpactEstimator = (data) => ({
 
 });
 
-// const datum = {
-
-//        "periodType": "days",
-//          "population": 7253232,
-//          "region": {
-//            "avgAge": 19.7,
-//            "avgDailyIncomeInUSD": 3,
-//            "avgDailyIncomePopulation": 0.69,
-//            "name": "Africa",
-//          },
-//          "reportedCases": 1973,
-//          "timeToElapse": 53,
-//          "totalHospitalBeds": 200978,
-//        }
-
-//  console.log(covid19ImpactEstimator(datum))
-// module.exports = covid19ImpactEstimator;
-export default covid19ImpactEstimator;
+module.exports = covid19ImpactEstimator;
